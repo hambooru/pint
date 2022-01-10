171d32f21e5d23bdaade01a39a397300af122625
nafunii <kawaii@duck.com>
2022-01-10 12:33:52 +0800

    feat: add focus color

--- popup.js
+++ popup.js
@@ -27,7 +27,8 @@ function setAccent() {
         document.querySelector(':root').style.setProperty('--color-btn-primary-bg', a1 + "ff", "important");
         document.querySelector(':root').style.setProperty('--color-btn-primary-hover-bg', a1 + "c5", "important");
         document.querySelector(':root').style.setProperty('--color-btn-primary-active-bg', a1 + "8a", "important");
-        document.querySelector(':root').style.setProperty('--color-btn-primary-disabled-bg', a1 + "54", "important")
+        document.querySelector(':root').style.setProperty('--color-btn-primary-focus-bg', a1 + "8a", "important");
+        document.querySelector(':root').style.setProperty('--color-btn-primary-disabled-bg', a1 + "54", "important");
         document.querySelector('.js-highlight-blob').style.setProperty('fill', a1 + "c5", "important");
         document.querySelector('.js-highlight-blob').style.setProperty('stroke', a1 + "ff", "important");
         for (let i = 0; i < document.querySelectorAll('.Progress-item.rounded-2').length; i++) {
