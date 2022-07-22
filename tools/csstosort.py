9d3eb1dc9b48ee906d724afa5b8217bf7e923f97
Restyled.io <commits@restyled.io>
2022-07-22 05:47:59 +0000

    Restyled by autopep8

--- tools/csstosort.py
+++ tools/csstosort.py
@@ -1,11 +1,11 @@
 with open('formatted.txt', 'a') as f:
-  x = open('colors.txt','r')
-  prev_line = ''
+    x = open('colors.txt', 'r')
+    prev_line = ''
 
-  for current_line in x:
-      if prev_line.split("=>")[0].strip() != current_line.split("=>")[0].strip():
-          f.write('\n')
-      f.write(current_line)
-      prev_line = current_line
+    for current_line in x:
+        if prev_line.split("=>")[0].strip() != current_line.split("=>")[0].strip():
+            f.write('\n')
+        f.write(current_line)
+        prev_line = current_line
 
-  x.close()
+    x.close()
